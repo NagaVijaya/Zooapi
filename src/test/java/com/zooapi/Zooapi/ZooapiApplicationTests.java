92ddc7d63dad9a794809c80241f7aa95fe8a62ab
NagaVijaya <nagavijayapatibandla@gmail.com>
2021-01-09 14:45:31 -0500

    initial commit

---
+++ src/test/java/com/zooapi/Zooapi/ZooapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zooapi.Zooapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class ZooapiApplicationTests {
+
+
+@Test
+	void smoke(){
+	assertTrue(true);
+}
+}
